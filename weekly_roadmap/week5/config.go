fe1d12c2396faf8fd4cf26209fdbcb367f285613
TruongDT <truongnopro1111@gmail.com>
2025-04-07 17:32:39 +0700

    completed: weekly_roadmap

--- weekly_roadmap/week5/config.go
+++ weekly_roadmap/week5/config.go
@@ -1,22 +1,22 @@
 package week5
 
 import (
-    "fmt"
-    "log"
+	"fmt"
+	"log"
 
-    "gorm.io/driver/mysql"
-    "gorm.io/gorm"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func ConnectMySQL() (*gorm.DB, error) {
-    // Thay đổi thông tin kết nối phù hợp với database của bạn
-    dsn := "username:password@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
-    db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-    if err != nil {
-        log.Printf("Error connecting to database: %v", err)
-        return nil, err
-    }
+	// Thay đổi thông tin kết nối phù hợp với database của bạn
+	dsn := "root:root@tcp(127.0.0.1:3306)/modern_tech?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Printf("Error connecting to database: %v", err)
+		return nil, err
+	}
 
-    fmt.Println("Successfully connected to MySQL using GORM!")
-    return db, nil
+	fmt.Println("Successfully connected to MySQL using GORM!")
+	return db, nil
 }
